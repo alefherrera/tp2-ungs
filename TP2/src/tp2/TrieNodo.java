94a6cb58f8c0eafd77978842a2d0950ce0f33059
alef.herrera <alef.herrera@gmail.com>
2014-06-16 17:09:45 +0000

    cambios

--- TP2/src/tp2/TrieNodo.java
+++ TP2/src/tp2/TrieNodo.java
@@ -1,22 +1,43 @@
 package tp2;
 
-public class TrieNodo<T1,T2> {
+import java.lang.reflect.Array;
 
-	public TrieNodo<T1,T2>[] nodos;
-	public Tupla<T1, T2> valor;
-	/*
-	public TrieNodo<T1,T2> getNodo(String s) {
-		TrieNodo<T1,T2> nodo = this;
-		for (int i = 0; i < s.length(); i++) {
-			int index = LOWERCASE.getIndex(s.charAt(i));
-			TrieNodo<T1,T2> child = nodo.nodos[index];
+public class TrieNodo<T1, T2> {
+
+	public TrieNodo<T1, T2>[] nodos;
+	public Character letra;
+	private TrieNodo<T1, T2> padre;
+	public Tupla<T1[], T2> valor;
+
+	public TrieNodo(Character l) {
+		letra = l;
+		nodos = (TrieNodo<T1, T2>[]) Array.newInstance(TrieNodo.class, 26);
+	}
+
+	public TrieNodo<T1, T2> buscar(T1[] s) {
+		TrieNodo<T1, T2> nodo = this;
+		for (int i = 0; i < s.length; i++) {
+			int index = (Character)s[i] - 'a';
+			TrieNodo<T1, T2> child = nodo.nodos[index];
 			if (child == null) {
-				// There is no such word
-				return null;
+				child = new TrieNodo<T1, T2>((Character)s[i]);
+				child.padre = this;
 			}
 			nodo = child;
 		}
 		return nodo;
-	}*/
+	}
 	
+	@Override
+	public String toString() {
+		String r = valor == null ? "" : valor.toString();
+		for (int i = 0; i < nodos.length; i++) {
+			if(nodos[i] != null)
+			{
+				r += nodos[i].toString();
+			}
+		}
+		return r;
+	}
+
 }
